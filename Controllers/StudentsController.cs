de5f2c2cd988dbb730300ec34f8c4b036ebda774
Nzubechukwu <nzubennamani@gmail.com>
2019-12-17 20:37:59 +0100

    add crud functionality

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -24,11 +24,59 @@ namespace classroom_api_c__tutorial.Controllers
             _context.Students.ToList();
 
         // GET by ID action
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetById(long id)
+        {
+           var student = await _context.Students.FindAsync(id);
+
+          if (student == null)
+        {
+          return NotFound();
+        }
+
+          return student;
+        }
 
         // POST action
+        [HttpPost]
+        public async Task<ActionResult<Student>> Create(Student student)
+        {
+           _context.Students.Add(student);
+           await _context.SaveChangesAsync();
+
+           return CreatedAtAction(nameof(GetById), new { id = student.Id }, student);
+        }
 
         // PUT action
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, Student student)
+        {
+           if (id != student.Id)
+        {
+           return BadRequest();
+        }
+
+           _context.Entry(student).State = EntityState.Modified;
+           await _context.SaveChangesAsync();
+
+           return NoContent();
+        }
 
         // DELETE action
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+        {
+            return NotFound();
+        }
+
+           _context.Students.Remove(student);
+           await _context.SaveChangesAsync();
+
+           return NoContent();
+        }
     }
 }
